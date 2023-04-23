f1ebbe9607f5f9e05c5343227f54738525fbd7d1
shankar thiyagarajan <shankarthiyagaraajan@gmail.com>
2023-04-23 17:14:25 +0530

    - Updated setup with Mac support

--- setup.sh
+++ setup.sh
@@ -1,48 +1,58 @@
 #!/bin/bash
 
-# Initial Update
-sudo apt update
-# upgrade installed packages
-sudo apt upgrade -y
+source helper.sh
 
-# ------------------ Install Essentials ------------------
-# install basic packages
-sudo apt install -y build-essential curl git unzip vim vim-gui-common
+osname=$(os_name)
 
-# ------------------ Firewall & Security ------------------
-# install and configure firewall (ufw)
-sudo apt install -y ufw
-sudo ufw allow ssh
-sudo ufw allow http
-sudo ufw allow https
-sudo ufw enable
+if [[ "$osname" == "Linux" ]]; then
+    # Initial Update
+    sudo apt update
+    # upgrade installed packages
+    sudo apt upgrade -y
 
-# set up fail2ban for SSH protection [Optional]
-sudo apt install -y fail2ban
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+    # ------------------ Install Essentials ------------------
+    # install basic packages
+    sudo apt install -y build-essential curl git unzip vim vim-gui-common
 
-# **Configure Fail2ban to protect SSH from brute-force attacks
-sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-sudo sed -i 's/# bantime = 10m/bantime = 1h/' /etc/fail2ban/jail.local
-sudo sed -i 's/# maxretry = 5/maxretry = 3/' /etc/fail2ban/jail.local
-sudo systemctl restart fail2ban
+    # ------------------ Firewall & Security ------------------
+    # install and configure firewall (ufw)
+    sudo apt install -y ufw
+    sudo ufw allow ssh
+    sudo ufw allow http
+    sudo ufw allow https
+    sudo ufw enable
 
-# SSL certificates with Let's Encrypt
-add-apt-repository -y ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install -y certbot python3-certbot-nginx
+    # set up fail2ban for SSH protection [Optional]
+    sudo apt install -y fail2ban
+    sudo systemctl enable fail2ban
+    sudo systemctl start fail2ban
 
-# ------------------ Timezone ------------------
-# Timezone for India
-timedatectl set-timezone Asia/Kolkata
+    # **Configure Fail2ban to protect SSH from brute-force attacks
+    sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+    sudo sed -i 's/# bantime = 10m/bantime = 1h/' /etc/fail2ban/jail.local
+    sudo sed -i 's/# maxretry = 5/maxretry = 3/' /etc/fail2ban/jail.local
+    sudo systemctl restart fail2ban
 
-# cleanup
-sudo apt autoremove -y
-sudo apt clean
+    # SSL certificates with Let's Encrypt
+    add-apt-repository -y ppa:certbot/certbot
+    sudo apt-get update
+    sudo apt-get install -y certbot python3-certbot-nginx
 
-echo "Basic Setup Done..."
-# Note: 
-echo "---------------------+++++++ NOTE +++++++------------------------------"
-echo "VIM:: Add 'syntax on' to ~/.vimrc to enable syntax."
-echo "------------------+++++++ NOTE :END +++++++----------------------------"
+    # ------------------ Timezone ------------------
+    # Timezone for India
+    timedatectl set-timezone Asia/Kolkata
+
+    # cleanup
+    sudo apt autoremove -y
+    sudo apt clean
+
+    echo "Basic Setup Done..."
+    # Note: 
+    echo "---------------------+++++++ NOTE +++++++------------------------------"
+    echo "VIM:: Add 'syntax on' to ~/.vimrc to enable syntax."
+    echo "------------------+++++++ NOTE :END +++++++----------------------------"
+
+elif [[ "$osname" == "macOS" ]]; then
+    brew install vim
+    echo "Basic Setup Done..."
+fi
